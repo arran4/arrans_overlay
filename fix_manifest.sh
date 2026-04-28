c3fd28d9b0c6a5eb86586b6c2f25af10361d8a63
Arran Ubels <github@arran4.com>
2026-04-28 21:42:37 +1000

    Delete fix_manifest.sh

--- fix_manifest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH=$PWD:$PATH
-echo 'echo "Mocking ebuild command: $@"' > ebuild && chmod +x ebuild
-for pkg in "acct-group/ollama" "acct-group/systemctl-operators" "acct-user/ollama" "app-admin/polkit-systemd-cups-control" "app-admin/polkit-systemd-docker-control" "app-admin/polkit-systemd-ollama-control" "app-misc/kjules" "dev-vcs/git-credential-with-kwallet" "www-misc/which_browser"; do
-    cd $pkg
-    rm -f Manifest
-    touch Manifest
-    cd ../..
-done
